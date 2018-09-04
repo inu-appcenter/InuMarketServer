086724c0c8d36647a2d02e839d269f8e359cb36e
임동완 <limson_95@naver.com>
2018-09-04 10:41:24 +0900

    error in report unhandled promise?

--- routes/report.js
+++ routes/report.js
@@ -3,13 +3,13 @@ const router = express.Router()
 const report = require('./model/report')
 
 
-router.post('/',async(req,res) => {
+router.post('/',(req,res) => {
     if(req.body.kind != "moonHee"){
         const nineOneOneReport = new report()
         nineOneOneReport.sender = req.body.senderId
         nineOneOneReport.context = req.body.kind +"분야로 '"+req.body.productId + "' 아이템 신고되엇습니다"
         nineOneOneReport.date = Date.now
-        nineOneOneReport.save(async(err)=>{
+        nineOneOneReport.save((err)=>{
             if(err) throw err
             res.json({ans:true})
         })
@@ -19,14 +19,14 @@ router.post('/',async(req,res) => {
         moonHeeReport.sender = req.body.senderId
         moonHeeReport.context = req.body.context
         moonHeeReport.date = Date.now
-        moonHeeReport.save(async(err) =>{
+        moonHeeReport.save((err) =>{
             if(err) throw err
             res.json({ans:true})
         })
     }
 })
 
-router.post('/read',async(req,res)=>{
+router.post('/read',(req,res)=>{
     report.find({}).exec((err,docs)=>{
         if(err) throw err
         res.send(docs[0])
