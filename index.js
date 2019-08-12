aac1225d9e87e3fd1df3fdcf750bd072d6732ad0
임동완 <limson_95@naver.com>
2019-08-12 19:11:49 +0900

    ...

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 const server = require("http").createServer(app)
 const io = require("socket.io")(server)
 
-io.on('connection',(socket)=>{
+io.of("/chat").on('connection',(socket)=>{
     console.log(socket.id)
 })
 
